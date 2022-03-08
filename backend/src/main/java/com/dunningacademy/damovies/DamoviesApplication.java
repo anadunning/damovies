086def1e22b61491274390fabe33662b3852fb6e
anadunning <anakpedrosa@hotmail.com>
2022-03-07 22:30:42 -0300

    Created project

---
+++ backend/src/main/java/com/dunningacademy/damovies/DamoviesApplication.java
@@ -0,0 +1,13 @@
+package com.dunningacademy.damovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamoviesApplication.class, args);
+	}
+
+}
