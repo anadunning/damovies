1e41fc2be588747b30fa69dd975b67630c82b091
anadunning <anakpedrosa@hotmail.com>
2022-03-13 23:54:26 -0300

    useParams

---
+++ frontend/src/components/FormCard/index.tsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
+import { BASE_URL } from "utils/requests";
+import "./styles.css";
+
+type Props = {
+  movieId : string;
+};
+
+function FormCard( { movieId }: Props) {
+
+  const [movie, setMovie] = useState<Movie>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies/${movieId}`)
+    .then((response) => {
+      setMovie(response.data);
+    });
+  }, [movieId]);
+
+//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>)
+        // event?.preventDefault();
+
+  return (
+    <div className="damovies-form-container">
+      <img
+        className="damovies-movie-card-image"
+        src={movie?.image}
+        alt={movie?.title}
+      />
+      <div className="damovies-card-bottom-container">
+        <h3>{movie?.title}</h3>
+        <form className="damovies-form">
+          <div className="form-group dasmovies-form-group">
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="form-group damovies-form-group">
+            <label htmlFor="score">Informe sua avaliação</label>
+            <select className="form-control" id="score">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="damovies-form-btn-container">
+            <button type="submit" className="btn btn-primary damovies-btn">
+              Salvar
+            </button>
+          </div>
+        </form>
+        <Link to="/">
+          <button className="btn btn-primary damovies-btn mt-3">
+            Cancelar
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default FormCard;
